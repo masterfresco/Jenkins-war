5de27ab8df453fab9951b13035092bc3ca804d41
asharanikatakam <asharanikatakam@gmail.com>
2020-03-11 13:31:38 +0530

    Sample app to deploy maven war on tomcat

---
+++ src/main/java/com/fresco/play/App.java
@@ -0,0 +1,9 @@
+package com.fresco.play;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Tomcat!" );
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome!!You have successfully deployed war file on tomcat!!</h2>
+</body>
+</html>
